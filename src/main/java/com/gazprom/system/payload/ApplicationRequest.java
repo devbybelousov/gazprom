63d88a852c4ceaf029cbea52e2a31bc049532971
devbybelousov <devbybelousov@yandex.ru>
2020-12-17 15:16:23 +0700

    Update ApplicationRequest.java

--- src/main/java/com/gazprom/system/payload/ApplicationRequest.java
+++ src/main/java/com/gazprom/system/payload/ApplicationRequest.java
@@ -3,11 +3,13 @@ package com.gazprom.system.payload;
 import com.gazprom.system.model.History;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class ApplicationRequest {
     private List<Long> usersId;
     private List<Long> privilegesId;
